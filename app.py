70c774b19548e4a7d1cfe07a3f1ce4efbe7e4b66
Adamumarahmad <adamumarahmad@gmail.com>
2026-06-22 15:40:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ model = load_model('Chicken_disease_model.h5')
 st.title('Chicken Disease Detection') 
 uploaded_file = st.file_uploader('Upload Chicken Image')
 if uploaded_file: 
-    image = Image.uploaded_file.convert('RGB')
+    image = Image.open(uploaded_file).convert('RGB')
     st.image(uploaded_file)
 if st.button('Predict'): 
     uploaded_file = uploaded_file.resize(224, 224)
-    uploaded_file_to_array = tf.keras.preprocessing.image.uploaded_file_to_array(uploaded_file)
+    uploaded_file_to_array = tf.keras.preprocessing.image.img_to_array(uploaded_file)
     uploaded_file_to_array = np.expand(uploaded_file_to_array, axis=0)
         
     prediction = model.predict(uploaded_file_to_array)
